5d2ed9d1581d41625624427c1a7f246270471b1f
Jianfei Wang <me@thinxer.com>
2013-07-31 01:17:10 +0000

    code clean

--- backend/src/aminerdata.cpp
+++ backend/src/aminerdata.cpp
@@ -4,9 +4,6 @@ using namespace std;
 using namespace sae::io;
 using namespace indexing;
 
-#define BM25_K 2.0
-#define BM25_B 0.75
-
 AMinerData::AMinerData(char const * prefix) {
     cerr << "loading aminer graph..." << endl;
     g.reset(MappedGraph::Open(prefix));
@@ -32,11 +29,7 @@ AMinerData::AMinerData(char const * prefix) {
     for (auto ai = g->Vertices(); ai->Alive(); ai->Next()) {
         if (ai->GlobalId() % 100000 == 0)
             cerr << "Parsing vertex: " << ai->GlobalId() << ", type: " << ai->Typename() << endl;
-        if (ai->Typename() == "Author") {
-            auto a = parse<Author>(ai->Data());
-            std::pair<std::string, int> author(a.names[0], ai->GlobalId());
-            author_index.map.insert(author);
-        } else if (ai->Typename() == "Publication"){
+        if (ai->Typename() == "Publication"){
             auto p = parse<Publication>(ai->Data());
             unique_ptr<TokenStream> stream(ArnetAnalyzer::tokenStream(p.title + " " + p.abstract));
             unordered_map<int, vector<short>> word_position;
@@ -57,10 +50,6 @@ AMinerData::AMinerData(char const * prefix) {
                 // insert a new posting item, global id cannot be larger than max int
                 pub_index[Term{word_id, 0}].insert(PostingItem{static_cast<int>(ai->GlobalId()), positions, score});
             }
-        } else if (ai->Typename() == "JConf") {
-            auto j = parse<JConf>(ai->Data());
-            std:pair<std::string, int> jconf (j.name, ai->GlobalId());
-            jconf_index.map.insert(jconf);
         }
     }
     cerr << "index built!" << endl;
