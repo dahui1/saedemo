1d12054089cb464d0682a059b7718772afc0f412
Jianfei Wang <me@thinxer.com>
2013-08-09 02:10:10 +0000

    update network intergration

--- frontend/network_integration.py
+++ frontend/network_integration.py
@@ -1,16 +1,25 @@
-linkedin_map = {}
-with open("/home/thinxer/aminer/aminer_linkedin.txt") as f:
-    for l in f:
-        aminer_id, inid, inurl = l.strip('\r\n').split('\t')
-        linkedin_map[int(aminer_id)] = (inid, inurl)
+link_types = dict(
+    homepage=0,
+    google_scholar=1,
+    linkedin=2,
+    microsoft_academic=3,
+    facebook=4,
+    twitter=5,
+    google_plus=6,
+    sina_weibo=7,
+    video_lecture=8
+)
+reverse_link_types = {v: k for k, v in link_types.items()}
+
+from collections import defaultdict
+link_maps = defaultdict(dict)
+import csv
+reader = csv.DictReader(open("/home/thinxer/aminer/author_links.csv"))
+for row in reader:
+    link_maps[int(row['AUTHORID'])][reverse_link_types[int(row['type'])]] = {
+        'url': row['LINK']
+    }
+
 
 def query(aminerid):
-    ret = {}
-
-    inid, inurl = linkedin_map.get(aminerid, (None, None))
-    if inurl:
-        ret['linkedin'] = {
-            'url': inurl
-        }
-
-    return ret
+    return link_maps[aminerid]
